5b7926648877290c94bf7d0e02a9ebba1accad8c
wetooa <derpykidyt@gmail.com>
2024-10-23 22:29:34 +0800

    stopping temporarily for docs

--- app/(main)/(open)/(routes)/automata/game-of-life/cell.tsx
+++ app/(main)/(open)/(routes)/automata/game-of-life/cell.tsx
@@ -6,7 +6,7 @@ import { CellState } from './page';
 function GOLCell(props: { cell: CellState, i: number, j: number, flipCellState: (i: number, j: number) => void }) {
   const { cell, i, j, flipCellState } = props;
   return (
-    <button className={`-outline-offset-2 outline-primary outline hover:bg-yellow-300 ${cell === CellState.ALIVE ? "bg-green-300" : ""}`} onClick={() => flipCellState(i, j)}></ button>
+    <button className={`-outline-offset-2 outline-1 outline-primary outline hover:bg-yellow-300 ${cell === CellState.ALIVE ? "bg-green-300" : ""}`} onClick={() => flipCellState(i, j)}></ button>
   )
 }
 
--- app/(main)/(open)/(routes)/automata/langtons-ant/cell.tsx
+++ app/(main)/(open)/(routes)/automata/langtons-ant/cell.tsx
@@ -6,7 +6,7 @@ import { CellState } from './page';
 function LACell(props: { cell: CellState, i: number, j: number, flipCellState: (i: number, j: number) => void, isAnt: boolean }) {
   const { cell, i, j, isAnt, flipCellState } = props;
   return (
-    <button className={`-outline-offset-2 outline-primary outline hover:bg-yellow-300 ${isAnt ? "bg-red-600" : cell === CellState.BLACK ? "bg-gray-500" : ""}`} onClick={() => flipCellState(i, j)}></ button>
+    <button className={`-outline-offset-2 outline-1 outline-primary outline hover:bg-yellow-300 ${isAnt ? "bg-red-600" : cell === CellState.BLACK ? "bg-gray-500" : ""}`} onClick={() => flipCellState(i, j)}></ button>
   )
 }
 
