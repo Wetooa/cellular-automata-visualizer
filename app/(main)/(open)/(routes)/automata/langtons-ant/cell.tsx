3d8fc739b124a3f6b6535a9d40f85779e8300b5e
wetooa <derpykidyt@gmail.com>
2024-10-24 00:25:24 +0800

    bug fix 4

--- app/(main)/(open)/(routes)/automata/langtons-ant/cell.tsx
+++ app/(main)/(open)/(routes)/automata/langtons-ant/cell.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { CellState } from './page';
 
 function LACell(props: { cell: CellState, i: number, j: number, flipCellState: (i: number, j: number) => void, isAnt: boolean }) {
-  const { cell, i, j, isAnt, flipCellState, antDirection } = props;
+  const { cell, i, j, isAnt, flipCellState } = props;
 
   return (
     <button className={`-outline-offset-2 outline-1 overflow-hidden text-nowrap outline-primary hover:bg-yellow-300 ${isAnt ? `bg-red-600 ` : cell === CellState.BLACK ? "bg-gray-500 outline" : "outline"}`} onClick={() => flipCellState(i, j)}> </ button>
